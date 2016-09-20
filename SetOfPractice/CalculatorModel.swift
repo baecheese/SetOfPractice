1b73ddbfab2064fe53fe6fbaf5c17dd3b752e8f7
배지영 <melissaeliza0@gmail.com>
2016-09-20 12:31:54 +0900

    MVC Desgin Pattern (modifying)

---
+++ SetOfPractice/CalculatorModel.swift
@@ -0,0 +1,30 @@
+//
+//  CalculatorModel.swift
+//  SetOfPractice
+//
+//  Created by 배지영 on 2016. 9. 20..
+//  Copyright © 2016년 baecheese. All rights reserved.
+//
+
+import Foundation
+
+// Calculator API
+class CalculatorModel {
+    
+    
+    func setOperand(operand:Double) {
+        
+    }
+    
+    // 수학기호
+    func performOperation(symbol:String) {
+        
+    }
+    
+    // Model을 사용하는 class가 result을 설정
+    var result:Double {
+        get {
+            return 0.0
+        }
+    }
+}
