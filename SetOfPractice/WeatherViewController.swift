703eed913991cc14a18bd0e700d029b0c1cefd78
배지영 <melissaeliza0@gmail.com>
2017-11-21 17:31:51 +0900

    cell 정보 연결

--- SetOfPractice/WeatherViewController.swift
+++ SetOfPractice/WeatherViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class WeatherCell: UITableViewCell {
-    @IBOutlet var weatherImage: UIImageView!
-    @IBOutlet weak var name = UILabel()
+    @IBOutlet var name: UILabel!
+    @IBOutlet var value: UILabel!
 }
 
 class WeatherViewController: UIViewController, WeatherManagerDelegate, UITableViewDelegate, UITableViewDataSource {
@@ -18,12 +18,14 @@ class WeatherViewController: UIViewController, WeatherManagerDelegate, UITableVi
     var cityParmeter = -1
     var selectArea = ""
     let weatherManager = WeatherManager()
+    let weatherInfo = WeatherInfo()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         weatherManager.delegate = self
         tableview.delegate = self
         tableview.dataSource = self
+        tableview.allowsSelection = false
     }
     
     override func viewWillLayoutSubviews() {
@@ -42,6 +44,7 @@ class WeatherViewController: UIViewController, WeatherManagerDelegate, UITableVi
     func getWeather(info: [String : Any]) {
         let weather = MyJSONPaser.sharedInstance.getByQuery(query: "weather.minutely", JSONDic: info)
         if let minutely = weather as? Array<[String:Any]> {
+            minutelyWeather = minutely[0]
             setBackgroundImage(info: minutely[0])
             setTopInformation(info: minutely[0])
             tableview.reloadData()
@@ -50,23 +53,23 @@ class WeatherViewController: UIViewController, WeatherManagerDelegate, UITableVi
     
     func setBackgroundImage(info:[String:Any]) {
         if let skyCode = MyJSONPaser.sharedInstance.getByQuery(query: "sky.code", JSONDic: info) as? String {
-            backgroundImage.image = UIImage(named: WeatherInfo().getSkyImageName(name: skyCode))
+            backgroundImage.image = UIImage(named: weatherInfo.getSkyImageName(name: skyCode))
             backgroundImage.alpha = 0.8
         }
     }
     
     func setTopInformation(info:[String:Any]) {
-        if let nowTemperature = MyJSONPaser.sharedInstance.getByQuery(query: WeatherInfo().querys["temperature.tc"]!, JSONDic: info) as? String,
+        if let nowTemperature = MyJSONPaser.sharedInstance.getByQuery(query: "temperature.tc", JSONDic: info) as? String,
             let skyCode = MyJSONPaser.sharedInstance.getByQuery(query: "sky.code", JSONDic: info) as? String {
             area.text = selectArea
-            sky.text = WeatherInfo().getSkyState(code: skyCode)
+            sky.text = weatherInfo.getSkyState(code: skyCode)
             temparature.text = nowTemperature
         }
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return weatherInfo.detailInfos.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -75,6 +78,16 @@ class WeatherViewController: UIViewController, WeatherManagerDelegate, UITableVi
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+        let name = weatherInfo.detailInfos[indexPath.row]
+        cell.name.text = name
+        cell.name.textColor = .black
+        if let value = MyJSONPaser.sharedInstance.getByQuery(query: weatherInfo.querys[name]!, JSONDic: minutelyWeather) as? String {
+            cell.value.text = value
+            cell.value.textColor = .black
+        }
+        else {
+            cell.value.text = "🌙"
+        }
         return cell
     }
     
